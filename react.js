a57f8de9687ab924c2e2f7237cc3ad6953bfb0e4
Raphael Hättich <11434@corp.competec.ch>
2019-07-15 12:27:17 +0200

    fixed the linting errors

--- react.js
+++ react.js
@@ -12,34 +12,34 @@
 module.exports = {
     extends: [
         './index.js',
-        'react-app'
+        'react-app',
     ],
 
     rules: {
-        'quotes': ['warn', 'single'],
-        'camelcase': 'off',
+        quotes: ['warn', 'single'],
+        camelcase: 'off',
         'no-case-declarations': 'off',
-        'indent': ['warn', 4, { 'SwitchCase': 1 }],
+        indent: ['warn', 4, {SwitchCase: 1}],
         'func-style': 'off',
         'no-extra-semi': 'warn',
         'no-debugger': 'warn',
         'no-process-env': 'off',
         'comma-dangle': ['warn', 'always-multiline'],
         'no-extra-parens': [
-        'warn',
-        'all',
-        {
-            'conditionalAssign': false,
-            'nestedBinaryExpressions': false,
-            'returnAssign': false,
-            'ignoreJSX': 'all'
-        }
+            'warn',
+            'all',
+            {
+                conditionalAssign: false,
+                nestedBinaryExpressions: false,
+                returnAssign: false,
+                ignoreJSX: 'all',
+            },
         ],
         'jsx-quotes': ['warn', 'prefer-double'],
         'react/sort-comp': 'error',
         'react/prop-types': 'warn',
         'react/prefer-stateless-function': 'warn',
         'react/prefer-es6-class': ['error', 'always'],
-        'react/default-props-match-prop-types': ['error', { 'allowRequiredDefaults': true }]
-    }
+        'react/default-props-match-prop-types': ['error', {allowRequiredDefaults: true}],
+    },
 };
