e461779400aed59f703d7f070d1df3f7090b3d85
Raphael Hättich <11434@corp.competec.ch>
2019-07-15 12:19:44 +0200

    added test module

---
+++ test.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview Competec ESLint Config for Tests
+ * @author Raphael Haettich / Competec
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Config Definition
+//------------------------------------------------------------------------------
+
+
+module.exports = {
+    extends: './index.js',
+
+    'globals': {
+      'logGlobalError': true,
+      'logger': true,
+      'CONFIG': true,
+      'CONST': true,
+      '$': true,
+      'jQuery': true,
+      'gtm': true,
+      'describe': true,
+      'it': true,
+      'expect': true,
+      'sinon': true,
+      'before': true,
+      'after': true,
+      'afterEach': true,
+      'beforeEach': true,
+      'R': true,
+      'stubElementOffsetHeight': true,
+      'mock': true,
+      'noop': true,
+      'stripSpecialCharacters': true,
+      'stubDomElementProperty': true
+    },
+
+    rules: {
+        'no-unused-expressions': 'off',
+        'one-var': 'off',
+        'comma-dangle': 'off',
+        'camelcase': 'off',
+        'one-var-declaration-per-line': 'off'
+    }
+};
