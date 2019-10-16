9a617780527614c3d2fea58d507dc4b64ac16d9f
jasondimitratos <871623-jasondimitratos@users.noreply.gitlab.com>
2019-10-16 15:47:32 +0200

    linting aktualisieren und modularisieren

---
+++ globals.js
@@ -0,0 +1,15 @@
+module.exports = {
+    globals: {
+        logGlobalError: true,
+        logger: true,
+        CONFIG: true,
+        CONST: true,
+        $: true,
+        jQuery: true,
+        gtm: true,
+        environments: true,
+        secureObject: true,
+        utag: true,
+        TMSHelper: true,
+    }
+};
