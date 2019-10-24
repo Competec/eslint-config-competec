d8d9a9b014c6866d9bec48175f162e12d6242deb
jasondimitratos <871623-jasondimitratos@users.noreply.gitlab.com>
2019-10-24 12:05:51 +0200

    refactor(SHOP3-22): try export file

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Competec ESLint Config
+ * @author Raphael Haettich / Jason Dimitratos / Competec
+ */
+import index from './src/index';
+export test from './src/test';
+export react from './src/react';
+
+export default index;
