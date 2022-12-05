3a7ebb183b0386530c04a6284d2a6777e39a73e4
Raphael Hättich <raphael@haettich.ch>
2022-12-05 08:33:40 +0100

    fix(DEV-10402): linting files fixed

--- typescript.js
+++ typescript.js
@@ -10,31 +10,34 @@ module.exports = {
     plugins: [
         '@typescript-eslint',
     ],
-    root: true,
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-    ],
-    files: [
-        '*.ts',
-        '*.tsx',
-    ],
     parserOptions: {
         warnOnUnsupportedTypeScriptVersion: false,
-    },
-    rules: {
-        'no-inner-declarations': 'off',
-        semi: 'off',
-        'no-extra-parens': 'off',
-        '@typescript-eslint/semi': [
-            'error',
-        ],
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/member-delimiter-style': 'off',
-        '@typescript-eslint/ban-ts-ignore': 'off',
     },
     settings: {
         typescript: true,
         node: true,
     },
+    parser: '@typescript-eslint/parser',
+    overrides: [
+        {
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+            ],
+            files: [
+                '*.ts',
+                '*.tsx',
+            ],
+            rules: {
+                'no-inner-declarations': 'off',
+                semi: 'off',
+                'no-extra-parens': 'off',
+                '@typescript-eslint/semi': [
+                    'error',
+                ],
+                '@typescript-eslint/explicit-function-return-type': 'off',
+                '@typescript-eslint/member-delimiter-style': 'off',
+                '@typescript-eslint/ban-ts-ignore': 'off',
+            },
+        },
+    ],
 };
