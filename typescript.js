4a590ddea4c83746fad3c4a423d76d6a599f85ea
Pascal Luginbühl <17087783+PascalLuginbuehl@users.noreply.github.com>
2023-10-17 09:45:16 +0200

    DEV-12121: add typescript import rules (#41)
    * DEV-12121: add typescript import rules
    
    * Feat (DEV-12121-typescript-import-rules): new version

--- typescript.js
+++ typescript.js
@@ -7,27 +7,34 @@
 //------------------------------------------------------------------------------
 
 module.exports = {
-    plugins: [
-        '@typescript-eslint',
-    ],
     parserOptions: {
         warnOnUnsupportedTypeScriptVersion: false,
     },
     settings: {
         typescript: true,
         node: true,
+        'import/resolver': {
+            typescript: true,
+            node: true,
+        },
     },
-    parser: '@typescript-eslint/parser',
     overrides: [
         {
+            parser: '@typescript-eslint/parser',
             extends: [
-                'plugin:@typescript-eslint/recommended',
+                // Error: Not uniquely imported, also gets imported by eslint-config-react-app
+                // 'plugin:@typescript-eslint/recommended',
+                'plugin:import/recommended',
+                'plugin:import/typescript',
             ],
             files: [
                 '*.ts',
                 '*.tsx',
             ],
             rules: {
+                'import/no-named-as-default': 'off',
+                'import/no-named-as-default-member': 'off',
+
                 'no-inner-declarations': 'off',
                 semi: 'off',
                 'no-extra-parens': 'off',
@@ -36,7 +43,69 @@ module.exports = {
                 ],
                 '@typescript-eslint/explicit-function-return-type': 'off',
                 '@typescript-eslint/member-delimiter-style': 'off',
+
                 '@typescript-eslint/ban-ts-ignore': 'off',
+                '@typescript-eslint/ban-ts-comment': 'off',
+
+                'flowtype/no-types-missing-file-annotation': 'off',
+
+                // Disabled due to incorrect errors https://typescript-eslint.io/rules/no-use-before-define/#how-to-use
+                'no-use-before-define': 'off',
+                '@typescript-eslint/no-use-before-define': 'error',
+
+                '@typescript-eslint/no-import-type-side-effects': 'error',
+
+                'no-unused-vars': 'off',
+                '@typescript-eslint/no-unused-vars': 'off',
+
+                // needs to be removed in the future: https://stackoverflow.com/questions/64170868/why-eslint-consider-jsx-or-some-react-types-undefined-since-upgrade-typescript
+                'no-undef': 'off',
+
+                // https://typescript-eslint.io/rules/no-shadow/#how-to-use
+                'no-shadow': 'off',
+                '@typescript-eslint/no-shadow': 'error',
+
+                'import/order': [
+                    'error',
+                    {
+                        alphabetize: {
+                            order: 'asc',
+                            caseInsensitive: false,
+                        },
+                        pathGroups: [
+                            {
+                                pattern: '{react,react-dom}',
+                                group: 'builtin',
+                                position: 'before',
+                            },
+                            {
+                                pattern: '{jest-axe,enzyme,react-dom/test-utils}',
+                                group: 'builtin',
+                                position: 'after',
+                            },
+                            {
+                                pattern: '*storybook*',
+                                group: 'builtin',
+                                position: 'after',
+                            },
+                            {
+                                pattern: '{@competec/**,b2c-components/**,b2c-resources/**}',
+                                group: 'external',
+                                position: 'after',
+                            },
+                        ],
+                        pathGroupsExcludedImportTypes: [],
+                        groups: [
+                            'builtin',
+                            'external',
+                            'parent',
+                            'sibling',
+                            'index',
+                            'type',
+                        ],
+                    },
+                ],
+                'import/newline-after-import': 'error',
             },
         },
     ],
